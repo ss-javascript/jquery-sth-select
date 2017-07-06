f594cbe5d824580a9818a30262cab74f972deeb0
Stanley Sathler <stanleysathlerpinto@gmail.com>
2017-07-06 11:54:55 -0300

    Preparing test environment

---
+++ test/sth-select.test.js
@@ -0,0 +1,13 @@
+/*
+ * Dependencies
+ */
+const assert = require("chai").assert;
+
+describe("SthSelect Test Suite", () => {
+
+	it("First test", () => {
+		let letter = "a";
+		assert.equal(letter, "a", "`letter` should be equal to `a`");
+	});
+
+});
